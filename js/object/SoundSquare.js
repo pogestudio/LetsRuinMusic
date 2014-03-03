21b89b81cc8d7168027c821fb720783db3baf297
klemenl <klemen.lilija@gmail.com>
2014-03-03 19:14:01 +0100

    Draws lots of boxes

--- js/object/SoundSquare.js
+++ js/object/SoundSquare.js
@@ -3,13 +3,15 @@ var SoundSquare = function(xPos, yPos, size, isHighlighted) {
     this.yPos = yPos;
     this.size = size;
     this.isHighlighted = isHighlighted;
+    this.setInteractive(true);
 };
 
-SoundSquare.prototype = new Pixi.graphics();
+SoundSquare.prototype = new PIXI.Graphics();
 SoundSquare.prototype.constructor = SoundSquare;
 
 SoundSquare.prototype.drawToCanvas = function() {
+    this.lineStyle(2, 0xFFFFFF, 1);
+    this.beginFill(0xFFFF0B, 0.5);
     //set color
-    drawRect(0,0,size,size);
+    this.drawRect(0, 0, this.size, this.size);
 };
-

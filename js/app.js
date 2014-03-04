d79e301c9ad16b4b47fa99895fe21c7cfefd8d5c
draggys <draggys@gmail.com>
2014-03-04 17:54:38 +0100

    synth_drum and new testdata

--- js/app.js
+++ js/app.js
@@ -8,14 +8,14 @@ $(function() {
 
     //Test data
     model.setCell(16, 16, 1);
-    model.setCell(18, 20, 1);
-    model.setCell(19, 24, 1);
+    model.setCell(17, 17, 1);
+    model.setCell(18, 18, 1);
     model.setCell(24, 30, 1);
     model.setCell(22, 19, 1);
     model.setCell(29, 29, 1);
     model.setCell(30, 27, 1);
     model.setCell(21, 19, 1);
-    model.setCell(26, 18, 2);
+    model.setCell(26, 18, 1);
     model.setCell(24, 16, 1);
     model.setCell(27, 17, 1);
     model.setCell(28, 18, 1);
@@ -26,20 +26,14 @@ $(function() {
     model.setCell(17, 23, 1);
     model.setCell(18, 24, 1);
 
-    model.setCell(22, 19, 3);
-    model.setCell(18, 20, 3);
-    model.setCell(23, 21, 3);
-    model.setCell(27, 22, 3);
-    model.setCell(19, 23, 3);
-    model.setCell(29, 24, 3);
+    model.setCell(28, 20, 2);
 
-
-    model.setCell(17, 27, 3);
-    model.setCell(17, 28, 3);
-    model.setCell(18, 29, 3);
-    model.setCell(20, 30, 3);
-    model.setCell(19, 31, 3);
-    model.setCell(21, 32, 3);
+    model.setCell(29, 20, 4);
+    model.setCell(30, 22, 4);
+    model.setCell(31, 23, 4);
+    model.setCell(32, 24, 4);
+    model.setCell(17, 25, 4);
+    model.setCell(18, 26, 4);
 
     model.notifyObservers();
 

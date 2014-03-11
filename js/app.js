b047555b215cf8fd6932710a449455620f9b685b
CA Ewerbring <caewerbring@gmail.com>
2014-03-11 16:20:56 +0100

    Fixed adding/removing cells

--- js/app.js
+++ js/app.js
@@ -7,9 +7,9 @@ $(function() {
     model.setSize(48, 48);
 
     //Test data
-    // model.setCell(16, 16, 1);
-    // model.setCell(17, 17, 1);
-    // model.setCell(18, 18, 1);
+    model.setCell(2, 2, 1);
+    model.setCell(5, 5, 1);
+    model.setCell(3, 8, 1);
     // model.setCell(19, 19, 1);
     // model.setCell(20, 20, 1);
     // model.setCell(24, 30, 1);
@@ -37,7 +37,6 @@ $(function() {
     // model.setCell(17, 25, 4);
     // model.setCell(18, 26, 4);
 
-    model.notifyObservers();
 
     var rendererContainer = new Renderer();
 
@@ -46,7 +45,7 @@ $(function() {
     //And create the needed controllers and views
     var canvasView = new CanvasView($("#canvasView"), model, rendererContainer, audioViewController);
     var canvasViewController = new CanvasViewController(canvasView, model);
-	
+
     //audioViewController.addObserver(canvasView);
 
     //And create the needed controllers and views
@@ -54,9 +53,16 @@ $(function() {
     //var miniMapViewController = new MiniMapViewController(miniMapView, model);
 
     var toolbarView = new ToolbarView($("#toolbarView"), audioModel, model);
-    var toolbarViewController = new ToolbarViewController(toolbarView, audioModel,model);
+    var toolbarViewController = new ToolbarViewController(toolbarView, audioModel, model);
 
     var connection = new Connection(model);
     connection.connect("ws://localhost:12001");
+
+    model.notifyObservers();
+
+    model.setCell(3, 8, 0);
+
+    model.notifyObservers();
+
 });
 window.navigate = {};
--- js/view/CanvasView.js
+++ js/view/CanvasView.js
@@ -1,10 +1,15 @@
-var CanvasView = function (containerDiv, model, rendererContainer, audioViewController) {
+var CanvasView = function(containerDiv, model, rendererContainer, audioViewController) {
 
     this.stage = rendererContainer.stage;
 
     //Factory needs parameters
-    this.cellFactory = new CellFactory();
-    this.cellContainer = new CellContainer(this.cellFactory, model, audioViewController);
+    var cellSize = 32;
+    var borderSize = 1;
+    this.cellFactory = new CellFactory(cellSize, borderSize);
+
+    var pixiSpriteBatchContainer = new PIXI.SpriteBatch();
+    rendererContainer.stage.addChild(pixiSpriteBatchContainer);
+    this.cellContainer = new CellContainer(this.cellFactory, model, audioViewController, pixiSpriteBatchContainer);
 
     model.addObserver(this);
 
@@ -16,30 +21,30 @@ var CanvasView = function (containerDiv, model, rendererContainer, audioViewCont
     rendererContainer.stage.mousemove = rendererContainer.stage.touchmove = this.onMouseMove;
     rendererContainer.stage.mouseup = rendererContainer.stage.mouseupoutside =
         rendererContainer.stage.touchend = rendererContainer.stage.touchendoutside = this.onMouseUp;
-}
+};
 
-CanvasView.prototype.onFrameRender = function (renderContainer, timeStep) {
+CanvasView.prototype.onFrameRender = function(renderContainer, timeStep) {
     this.cellContainer.updateAnimations(timeStep);
-}
+};
 
 //Notification from model
-CanvasView.prototype.update = function (model) {
+CanvasView.prototype.update = function(model) {
 
-}
+};
 
-CanvasView.prototype.onMouseDown = function (data) {
+CanvasView.prototype.onMouseDown = function(data) {
     //if in the middle
     //  get pos from screen
     //  get/set cell
 
     //else
     //  start drag canvas magic
-}
+};
 
-CanvasView.prototype.onMouseMove = function (data) {
+CanvasView.prototype.onMouseMove = function(data) {
     //drag canvas
-}
+};
 
-CanvasView.prototype.onMouseUp = function (data) {
+CanvasView.prototype.onMouseUp = function(data) {
     //stop drag canvas magic
-}
+};

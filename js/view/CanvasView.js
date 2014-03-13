b99e6f93023784e0041511fe61fa506e8b6ae668
Daniel <daniel.maansson@gmail.com>
2014-03-13 01:06:39 +0100

    Smooth align to overlay

--- js/view/CanvasView.js
+++ js/view/CanvasView.js
@@ -1,6 +1,8 @@
 var CanvasView = function(containerDiv, model, rendererContainer, audioViewController) {
     this.model = model;
     this.stage = rendererContainer.stage;
+    this.moveX = 0.0;
+    this.moveY = 0.0;
 
     //Factory needs parameters
     this.cellSize = 32;
@@ -39,10 +41,26 @@ var CanvasView = function(containerDiv, model, rendererContainer, audioViewContr
 
     this.mouseDrag = new MouseDrag();
     this.mouseDrag.addListener(this);
+
+    //Align overview at startup
+    this.onDragStop(null, null);
 };
 
 CanvasView.prototype.onFrameRender = function(renderContainer, timeStep) {
     this.cellContainer.updateAnimations(timeStep);
+
+
+    var moveX = this.moveX * timeStep * 10.0;
+    var moveY = this.moveY * timeStep * 10.0;
+
+    this.cellContainer.spriteBatchContainer.position.x += moveX;
+    this.cellContainer.spriteBatchContainer.position.y += moveY;
+ 
+    this.background.tilingSprite.tilePosition.x += moveX;
+    this.background.tilingSprite.tilePosition.y += moveY;
+
+    this.moveX -= moveX;
+    this.moveY -= moveY;
 };
 
 //Notification from model
@@ -98,16 +116,24 @@ CanvasView.prototype.onDragStop = function (point, move) {
     //TODO: Snap to overlay
     var diffX = (this.cellContainer.spriteBatchContainer.position.x - this.overlayContainer.x) % this.cellSize;
     var diffY = (this.cellContainer.spriteBatchContainer.position.y - this.overlayContainer.y) % this.cellSize;
-    
-    this.cellContainer.spriteBatchContainer.position.x -= diffX;
-    this.cellContainer.spriteBatchContainer.position.y -= diffY;
 
-    this.background.tilingSprite.tilePosition.x -= diffX;
-    this.background.tilingSprite.tilePosition.y -= diffY;
+    diffX = (diffX + this.cellSize) % this.cellSize;
+    diffY = (diffY + this.cellSize) % this.cellSize;
+
+    var otherDiffX = diffX - this.cellSize;
+    var otherDiffY = diffY - this.cellSize;
+    
+    if (diffX > -otherDiffX)
+        diffX = otherDiffX;
+    if (diffY > -otherDiffY)
+        diffY = otherDiffY;
+
+    this.moveX = -diffX;
+    this.moveY = -diffY;
 
     var globalPos = this.cellContainer.getGlobalPosFromScreenPos(
-        this.overlayContainer.x,
-        this.overlayContainer.y);
+        this.overlayContainer.x + diffX,
+        this.overlayContainer.y + diffY);
 
     this.model.x = globalPos.x;
     this.model.y = globalPos.y;

c84f25a98846706b3f92e4d410b8bcb9c69cc27e
CA Ewerbring <caewerbring@gmail.com>
2014-03-21 18:31:40 +0100

    Title and FPS counter

--- js/graphics/Renderer.js
+++ js/graphics/Renderer.js
@@ -9,15 +9,15 @@
     this.renderer.view.style.top = "0px";
     this.renderer.view.style.left = "0px";
 
-    this.stats = new Stats();
-    this.stats.setMode(1); // 0: fps, 1: ms
+    // this.stats = new Stats();
+    // this.stats.setMode(1); // 0: fps, 1: ms
 
-    // Align top-left
-    this.stats.domElement.style.position = 'absolute';
-    this.stats.domElement.style.left = '0px';
-    this.stats.domElement.style.top = '200px';
+    // // Align top-left
+    // this.stats.domElement.style.position = 'absolute';
+    // this.stats.domElement.style.left = '0px';
+    // this.stats.domElement.style.top = '200px';
 
-    document.body.appendChild(this.stats.domElement);
+    // document.body.appendChild(this.stats.domElement);
 
     //Auto resize canvas
     //-5 removes scrollbars, quite a hack
@@ -32,7 +32,7 @@
 
     this.frameListeners = [];
     function animate() {
-        self.stats.begin();
+        //self.stats.begin();
 
         self.frameListeners.forEach(function (listener) {
             listener.onFrameRender(self, 0.0166); //TODO: add real timestep
@@ -40,7 +40,7 @@
 
         self.renderer.render(self.stage);
         requestAnimFrame(animate);
-        self.stats.end();
+        //self.stats.end();
     }
 }
 
